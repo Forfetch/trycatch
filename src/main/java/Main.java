a27de04313f23ef7918e83784d108165c5d18a0e
mabuttta <mabutta@gmail.com>
2023-05-12 21:09:03 +0300

    ver 1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        try {
+            System.out.println("Enter first number ");
+            int first = scanner.nextInt();
+            try {
+                System.out.println("Enter second number ");
+                int sec = scanner.nextInt();
+                scanner.close();
+                int res = first/sec;
+                System.out.println("Result " + res);
+            } catch (ArithmeticException e) {
+                System.out.println("Division by zero!!!!!");
+            }
+
+        } catch (Exception ex ){
+            System.out.println("Invalid number");
+        }
     }
 }
